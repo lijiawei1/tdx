eb243b4ac71ca3ea9e6d3ba2011c39479a3ace15
injoyai <1113655791@qq.com>
2025-02-21 14:25:14 +0800

    增加拓展,封装每日更新代码和工作日信息

---
+++ codes.go
@@ -0,0 +1,185 @@
+package tdx
+
+import (
+	"github.com/injoyai/logs"
+	"github.com/injoyai/tdx/protocol"
+	"github.com/robfig/cron/v3"
+	"os"
+	"path/filepath"
+	"time"
+	"xorm.io/xorm"
+)
+
+func NewCodes(c *Client, filename string) (*Codes, error) {
+
+	//如果文件夹不存在就创建
+	dir, _ := filepath.Split(filename)
+	_ = os.MkdirAll(dir, 0777)
+
+	//连接数据库
+	db, err := xorm.NewEngine("sqlite", filename)
+	if err != nil {
+		return nil, err
+	}
+	db.DB().SetMaxOpenConns(1)
+	if err := db.Sync2(new(CodeModel)); err != nil {
+		return nil, err
+	}
+
+	cc := &Codes{
+		Client: c,
+		db:     db,
+		Codes:  nil,
+	}
+
+	//设置定时器,每天早上9点更新数据
+	task := cron.New(cron.WithSeconds())
+	task.AddFunc("0 0 9 * * *", func() {
+		for i := 0; i < 3; i++ {
+			if err := cc.Update(); err == nil {
+				return
+			}
+			logs.Err(err)
+			<-time.After(time.Minute * 5)
+		}
+	})
+	task.Start()
+
+	return cc, cc.Update()
+}
+
+type Codes struct {
+	*Client                       //客户端
+	db      *xorm.Engine          //数据库实例
+	Codes   map[string]*CodeModel //股票缓存
+}
+
+// GetName 获取股票名称
+func (this *Codes) GetName(code string) string {
+	if v, ok := this.Codes[code]; ok {
+		return v.Name
+	}
+	return "未知"
+}
+
+// GetStocks 获取股票代码,sh6xxx sz0xx sz30xx
+func (this *Codes) GetStocks() []string {
+	ls := []string(nil)
+	for k, _ := range this.Codes {
+		if protocol.IsStock(k) {
+			ls = append(ls, k)
+		}
+	}
+	return ls
+}
+
+func (this *Codes) Update() error {
+	codes, err := this.Code(false)
+	if err != nil {
+		return err
+	}
+	codeMap := make(map[string]*CodeModel)
+	for _, code := range codes {
+		codeMap[code.Exchange+code.Code] = code
+	}
+	this.Codes = codeMap
+	return nil
+}
+
+// Code 更新股票并返回结果
+func (this *Codes) Code(byDatabase bool) ([]*CodeModel, error) {
+
+	//2. 查询数据库所有股票
+	list := []*CodeModel(nil)
+	if err := this.db.Find(&list); err != nil {
+		return nil, err
+	}
+
+	//如果是从缓存读取,则返回结果
+	if byDatabase {
+		return list, nil
+	}
+
+	mCode := make(map[string]*CodeModel, len(list))
+	for _, v := range list {
+		mCode[v.Code] = v
+	}
+
+	//3. 从服务器获取所有股票代码
+	insert := []*CodeModel(nil)
+	update := []*CodeModel(nil)
+	for _, exchange := range []protocol.Exchange{protocol.ExchangeSH, protocol.ExchangeSZ} {
+		resp, err := this.Client.GetCodeAll(exchange)
+		if err != nil {
+			return nil, err
+		}
+		for _, v := range resp.List {
+			if _, ok := mCode[v.Code]; ok {
+				if mCode[v.Code].Name != v.Name {
+					mCode[v.Code].Name = v.Name
+					update = append(update, &CodeModel{
+						Name:     v.Name,
+						Code:     v.Code,
+						Exchange: exchange.String(),
+					})
+				}
+			} else {
+				code := &CodeModel{
+					Name:     v.Name,
+					Code:     v.Code,
+					Exchange: exchange.String(),
+				}
+				insert = append(insert, code)
+				list = append(list, code)
+			}
+		}
+	}
+
+	//4. 插入或者更新数据库
+	err := this.SessionFunc(func(session *xorm.Session) error {
+		for _, v := range insert {
+			if _, err := session.Insert(v); err != nil {
+				return err
+			}
+		}
+		for _, v := range update {
+			if _, err := session.Where("Code=?", v.Code).Cols("Name").Update(v); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return list, nil
+
+}
+
+func (this *Codes) SessionFunc(fn func(session *xorm.Session) error) error {
+	session := this.db.NewSession()
+	defer session.Close()
+	if err := session.Begin(); err != nil {
+		session.Rollback()
+		return err
+	}
+	if err := fn(session); err != nil {
+		session.Rollback()
+		return err
+	}
+	if err := session.Commit(); err != nil {
+		session.Rollback()
+		return err
+	}
+	return nil
+}
+
+type CodeModel struct {
+	ID       int64  `json:"id"`                      //主键
+	Name     string `json:"name"`                    //名称,有时候名称会变,例STxxx
+	Code     string `json:"code" xorm:"index"`       //代码
+	Exchange string `json:"exchange" xorm:"index"`   //交易所
+	EditDate int64  `json:"editDate" xorm:"updated"` //修改时间
+	InDate   int64  `json:"inDate" xorm:"created"`   //创建时间
+}
---
+++ workday.go
@@ -0,0 +1,144 @@
+package tdx
+
+import (
+	_ "github.com/glebarez/go-sqlite"
+	"github.com/injoyai/base/maps"
+	"github.com/injoyai/logs"
+	"github.com/robfig/cron/v3"
+	"os"
+	"path/filepath"
+	"time"
+	"xorm.io/xorm"
+)
+
+func NewWorkday(c *Client, filename string) (*Workday, error) {
+
+	//如果文件夹不存在就创建
+	dir, _ := filepath.Split(filename)
+	_ = os.MkdirAll(dir, 0777)
+
+	//连接数据库
+	db, err := xorm.NewEngine("sqlite", filename)
+	if err != nil {
+		return nil, err
+	}
+	db.DB().SetMaxOpenConns(1)
+	if err := db.Sync2(new(WorkdayModel)); err != nil {
+		return nil, err
+	}
+
+	w := &Workday{
+		Client: c,
+		db:     db,
+		cache:  maps.NewBit(),
+	}
+
+	//设置定时器,每天早上9点更新数据,8点多获取不到今天的数据
+	task := cron.New(cron.WithSeconds())
+	task.AddFunc("0 0 9 * * *", func() {
+		for i := 0; i < 3; i++ {
+			if err := w.Update(); err == nil {
+				return
+			}
+			logs.Err(err)
+			<-time.After(time.Minute * 5)
+		}
+	})
+	task.Start()
+
+	return w, w.Update()
+}
+
+type Workday struct {
+	*Client
+	db    *xorm.Engine
+	cache maps.Bit
+}
+
+// Update 更新
+func (this *Workday) Update() error {
+	//获取平安银行的日K线,用作历史是否节假日的判断依据
+	//判断日K线是否拉取过
+
+	//获取全部工作日
+	all := []*WorkdayModel(nil)
+	if err := this.db.Find(&all); err != nil {
+		return err
+	}
+	var lastWorkday = &WorkdayModel{}
+	if len(all) > 0 {
+		lastWorkday = all[len(all)-1]
+	}
+	for _, v := range all {
+		this.cache.Set(uint64(v.Unix), true)
+	}
+
+	now := time.Now()
+	if lastWorkday == nil || lastWorkday.Unix < IntegerDay(now).Unix() {
+		resp, err := this.Client.GetKlineDayAll("sz000001")
+		if err != nil {
+			logs.Err(err)
+			return err
+		}
+
+		this.SessionFunc(func(session *xorm.Session) error {
+			for _, v := range resp.List {
+				if unix := v.Time.Unix(); unix > lastWorkday.Unix {
+					_, err = session.Insert(&WorkdayModel{Unix: unix, Date: v.Time.Format("20060102"), Is: true})
+					if err != nil {
+						return err
+					}
+					this.cache.Set(uint64(unix), true)
+				}
+			}
+			return nil
+		})
+
+	}
+	return nil
+}
+
+// Is 是否是工作日
+func (this *Workday) Is(t time.Time) bool {
+	return this.cache.Get(uint64(IntegerDay(t).Add(time.Hour * 15).Unix()))
+}
+
+// TodayIs 今天是否是工作日
+func (this *Workday) TodayIs() bool {
+	return this.Is(time.Now())
+}
+
+func (this *Workday) SessionFunc(fn func(session *xorm.Session) error) error {
+	session := this.db.NewSession()
+	defer session.Close()
+	if err := session.Begin(); err != nil {
+		session.Rollback()
+		return err
+	}
+	if err := fn(session); err != nil {
+		session.Rollback()
+		return err
+	}
+	if err := session.Commit(); err != nil {
+		session.Rollback()
+		return err
+	}
+	return nil
+}
+
+// WorkdayModel 工作日
+type WorkdayModel struct {
+	ID   int64  `json:"id"`   //主键
+	Unix int64  `json:"unix"` //时间戳
+	Date string `json:"date"` //日期
+	Is   bool   `json:"is"`   //是否是工作日
+}
+
+func (this *WorkdayModel) TableName() string {
+	return "workday"
+}
+
+func IntegerDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+}
