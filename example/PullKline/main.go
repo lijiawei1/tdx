6062da9cb23d5d4df353f5ad5984c2b85b0ba293
injoyai <1113655791@qq.com>
2025-03-18 14:55:15 +0800

    测试拓展pullkline

---
+++ example/PullKline/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"github.com/injoyai/logs"
+	"github.com/injoyai/tdx"
+	"github.com/injoyai/tdx/extend"
+)
+
+func main() {
+
+	m, err := tdx.NewManage(nil)
+	logs.PanicErr(err)
+
+	err = extend.NewPullKline([]string{"sz000001"}, []string{"year"}, "./", 1).Run(context.Background(), m)
+	logs.PanicErr(err)
+
+}
